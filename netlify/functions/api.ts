9706458b64d94f0c76e3d1f59dbce8c020704de9
Adam Polczynski <adam@minitmoney.com>
2025-06-04 14:15:22 +0200

    parse body manually

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -4,6 +4,22 @@ import { webhookGet, webhookPost } from "../lib/webhooks";
 
 const api = express();
 api.use(express.json());
+import { Buffer } from "buffer";
+
+api.use((req, res, next) => {
+  let data = "";
+  req.on("data", (chunk) => {
+    data += chunk;
+  });
+  req.on("end", () => {
+    try {
+      req.body = JSON.parse(data); // Manually parse to JSON
+    } catch (e) {
+      req.body = data; // fallback
+    }
+    next();
+  });
+});
 
 const router = Router();
 router.post("/webhook", webhookPost);
