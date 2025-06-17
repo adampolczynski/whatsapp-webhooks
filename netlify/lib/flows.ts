0f069de2c20a7d0f5a54a70213ad9c64d5fd62be
Adam Polczynski <adam@minitmoney.com>
2025-06-17 13:14:49 +0200

    a

--- netlify/lib/flows.ts
+++ netlify/lib/flows.ts
@@ -6,8 +6,10 @@ export const signUpFlow = (mode: "sign_up" | "sign_in") => ({
   interactive: {
     type: "flow",
     header: {
-      type: "Image",
-      src: logoBase64,
+      type: "image",
+      image: {
+        link: "https://scontent.fpoz3-1.fna.fbcdn.net/v/t39.30808-6/295057881_548111403567075_6430731063966312323_n.png?_nc_cat=103&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=VarMZ7FZREYQ7kNvwF9OM5I&_nc_oc=Adk1QVl5B2gO-Nwv9UIBr7LLGHo_AL4u5Mo_yYc5E7UebHI_2t8tCSbzrJCCI0RxlIY&_nc_zt=23&_nc_ht=scontent.fpoz3-1.fna&_nc_gid=tRzZgMWo0k8DnnGOrfIMLw&oh=00_AfM0L3hbFZIOcm2rKHuW7ateP2fJsWuyNTOwiTku3rW0ug&oe=6857084E",
+      },
     },
     body: {
       text: `${mode === "sign_up" ? "Sign up" : "Login"} to continue`,
